4acccdd39fa8e53172c9fb4ede0141e38d95aec5
danger-academy <robert.tarkenton@gmail.com>
2019-02-12 20:15:45 -0500

    Update friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,34 +1,23 @@
-var people = [
-    {
-      routeName: "",
-      name: "",
-      photo: "",
-      scores: []
-    },
-    {
-      routeName: "",
-      name: "",
-      photo: "",
-      scores: []
-    },
-    {
-      routeName: "",
-      name: "",
-      photo: "",
-      scores: []
-    },
-    {
-      routeName: "",
-      name: "",
-      photo: "",
-      scores: []
-    },
-    {
-      routeName: "",
-      name: "",
-      photo: "",
-      scores: []
-    }
-  ];
-  
-  module.exports = people;
+var people = [{
+    name: "test 01",
+    photo: "http://www.vincegolangco.com/wp-content/uploads/2010/12/chain-saw-at-facebook.jpg",
+    scores: ["3", "3", "3", "3", "3", "3", "3", "3", "3", "3"]
+  },
+  {
+    name: "test 02",
+    photo: "http://profilepicturesdp.com/wp-content/uploads/2018/06/generic-female-profile-picture-8.jpg",
+    scores: ["4", "4", "4", "2", "2", "2", "4", "4", "4", "4"]
+  },
+  {
+    name: "test 03",
+    photo: "http://www.wfmu.org/images/generic_avatar_300.png",
+    scores: ["4", "2", "4", "2", "4", "5", "1", "4", "5", "1"]
+  },
+  {
+    name: "test 05",
+    photo: "http://www.bukuraleigh.com/wp-content/uploads/2015/01/generic_female.jpg",
+    scores: ["2", "2", "2", "2", "2", "2", "2", "5", "5", "3"]
+  }
+]
+
+module.exports = people;
